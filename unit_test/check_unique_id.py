592764cb003b653894cb829012327fa2d069c643
kieuvinhh <vinhkg2333@gmail.com>
2024-07-21 13:05:23 +0700

    modify: test ci

--- unit_test/check_unique_id.py
+++ unit_test/check_unique_id.py
@@ -5,7 +5,7 @@ import os
 
 
 class DataframeTest(unittest.TestCase):
-    folder_gold_path = "../data/gold"
+    folder_gold_path = os.getenv('FOLDER_GOLD_PATH', './data/gold')
 
     def test_unique_id(self):
         parquet_files = {
